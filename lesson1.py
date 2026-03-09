3e71b3776d13d46b28e4f8737e2878586af02f7a
Jon.ONeill <jonathan.oneill@edesk.com>
2026-03-09 19:52:49 +0000

    Lesson 2 work

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+a=123
+b=456
+
+print(a*b)
+
+print("This is a string")
+
+# Comments will look like this. All of this can be customised.
+
+print(6.1+1.3)
+print(1.3+1.3+1.3)
+
+print(type(a))
+
+name=input("Gimme your name")
+print("Hello"+name)
---
+++ lesson2.py
@@ -0,0 +1,64 @@
+#name=input("Hello what is your name?\n")
+# live=input("\nWhere do you live?\n")
+
+# print("Aha, welcome "+name+" and you are from "+live+", awesome!")
+
+'''
+if name=="Chuck Norris":
+    print("Hello Chuck")
+
+print("END")
+
+
+# [2:53 PM] Mariela Mambrin (50054372)
+name=input("what is your name?")
+
+location=input("where are you from?")
+
+print("Hello" + name + "from" + location)
+ 
+
+name=input("What is your name?")
+if name=="Steve Hind":
+        print("Is it true that you have an aggresive accent and you always give a Glasgow kiss to strangers?")
+
+        
+
+
+name=input("Please could I have you name?\n")
+
+if name == "Chuck Norris":
+    print("Hello chuck")
+elif name == "Jon":
+    print("Might want to change your name to Chuck Norris")
+elif name == "Steve Irwin":
+    print("Crikey")
+else:
+    print("Hello, "+name)
+
+print("Program")
+
+'''
+
+
+get_program=input("What is your favourite program?\n")
+
+if (get_program=="Kardashians"):
+        print("Aha, a person of fine taste")
+        favourite_kard=input("Who's your favourite Kardashians.")
+        
+        if favourite_kard == "Kim":
+            print("Cool beans")
+        elif favourite_kard == "Khloe":
+            print("Cool, I dont know who that is")
+        elif favourite_kard == "That other Kardashian":
+            print("Me neither...")
+        elif favourite_kard == "Garak":
+             print("They are a Cardassian, not a Kardashian... go back to Star Trek")
+        else:
+             print("You cant be serious")
+
+elif get_program=="Doctor Who":
+     print("You're in the wrong if/else loop")
+else:
+     print("Never heard of",get_program)
