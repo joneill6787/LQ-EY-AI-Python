8c12b95b8041e10cc99371b0410b6cb6df648462
Jon.ONeill <jonathan.oneill@edesk.com>
2026-03-11 19:37:11 +0000

    Rounding off day

--- LQ-EY-AI-10-03-Work/python_panda.py
+++ LQ-EY-AI-10-03-Work/python_panda.py
@@ -2,11 +2,16 @@ import pandas as pd
 
 
 data = {
-    "Product":["Laptop","Phone","Tablet"],
-    "Price":[900,500,300],
-    "Sales":[50,120,80]
-
+    "Product":["Laptop","Phone","Tablet","Broom"],
+    "Price":[900,500,300,400],
+    "Sales":[50,120,80,200],
+    "Status":["Used","New","Used-Good","New"]
 }
 
 df = pd.DataFrame(data)
 print(df)
+
+cf = pd.read_csv("Student.csv")
+
+print()
+print(cf)
