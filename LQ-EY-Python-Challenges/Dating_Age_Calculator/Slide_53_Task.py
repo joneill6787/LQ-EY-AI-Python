529805e905a5d8b54c4a63a0a49c7e44989ea356
Jon.ONeill <jonathan.oneill@edesk.com>
2026-03-13 08:38:34 +0000

    completed Age calculator task 3

--- LQ-EY-Python-Challenges/Dating_Age_Calculator/Slide_53_Task.py
+++ LQ-EY-Python-Challenges/Dating_Age_Calculator/Slide_53_Task.py
@@ -15,20 +15,73 @@ Part 2:
 
     Modify the program so that it also works out the oldest person you can date.
 
+
+Part 3:
+
+    For every 1 million you have in the bank, you can date someone a year younger.
+        Modify your algorithm so that it takes into account your
+        net worth in millions.
 '''
 
-age=[18,25,30,45,60,80,100]
 
-for i in age:
+def testAgeCalculator():
+    age=[18,25,30,45,60,80,100]
 
-    dateAgeYoung = round((i/2)+7)
-    dateAgeOld = round((i*2)-7)
+    for i in age:
+
+        dateAgeYoung = round((i/2)+7)
+        dateAgeOld = round((i*2)-7)
+
+        if dateAgeYoung < 18:
+            print(f"You are {i} the youngest person you can date is the following age 18")
+            print(f"You are {i} the oldest person you can date is the following age {dateAgeOld}")
+        else:
+            print(f"You are {i} the youngest person you can date is the following age {dateAgeYoung}")
+            print(f"You are {i} the oldest person you can date is the following age {dateAgeOld}")
+
+        print()
+
+
+def datingAgeCalculator():
+
+        # This function will resolve tasks 1 and 2
+
+    userAge= int(input("Please could I have your age so we can calculate who you can date?\n"))
+
+    dateAgeYoung = round((userAge/2)+7)
+    dateAgeOld = round((userAge*2)-7)
+
+    if userAge < 18:
+        print(f"You are {userAge} the youngest person you can date is the following age 18")
+        print(f"You are {userAge} the oldest person you can date is the following age {dateAgeOld}")
+    else:
+        print(f"You are {userAge} the youngest person you can date is the following age {dateAgeYoung}")
+        print(f"You are {userAge} the oldest person you can date is the following age {dateAgeOld}")
+
+
+def datingAgeMoneyCalculator():
+
+        # This function will resolve tasks 
+
+    userAge= int(input("Please could I have your age so we can calculate who you can date?\n"))
+    userWealth= int(input("And how much do you earn in millions per year?\n"))
+
+    if(userWealth>100):
+        print("Well aint you rich? Can you spare me a few million?")
+
+    dateAgeYoung = round((userAge/2)+7-userWealth)
+    dateAgeOld = round((userAge*2)-7-userWealth)
 
     if dateAgeYoung < 18:
-        print(f"You are {i} the youngest person you can date is the following age 18")
-        print(f"You are {i} the oldest person you can date is the following age {dateAgeOld}")
+        print(f"You are {userAge} and have {userWealth} million in the bank")
+        print(f"The youngest person you can date is the following age: 18")
+        if dateAgeOld < 18:
+            print(f"The oldest person you can date is the following age: 18")
+        else:
+            print(f"The oldest person you can date is the following age: {dateAgeOld}")
     else:
-        print(f"You are {i} the youngest person you can date is the following age {dateAgeYoung}")
-        print(f"You are {i} the oldest person you can date is the following age {dateAgeOld}")
+        print(f"You are {userAge} the youngest person you can date is the following age {dateAgeYoung}")
+        print(f"You are {userAge} the oldest person you can date is the following age {dateAgeOld}")
 
-    print()
+
+datingAgeMoneyCalculator()
